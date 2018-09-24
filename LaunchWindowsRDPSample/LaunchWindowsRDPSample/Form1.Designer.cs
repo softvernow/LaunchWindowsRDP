ab03098122a2f35e9234ed491d11875366c8d2c5
Softvernow <38967263+softvernow@users.noreply.github.com>
2018-09-24 11:46:08 -0400

    Launch Windows Remote Desktop Using C#
    On this Tutorial we will show you how to Launch Windows Remote Desktop Using C# .NET application.

---
+++ LaunchWindowsRDPSample/LaunchWindowsRDPSample/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LaunchWindowsRDPSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMachineName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRDP = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMachineName
+            // 
+            this.txtMachineName.Location = new System.Drawing.Point(66, 21);
+            this.txtMachineName.Name = "txtMachineName";
+            this.txtMachineName.PasswordChar = '*';
+            this.txtMachineName.Size = new System.Drawing.Size(183, 20);
+            this.txtMachineName.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Machine:";
+            // 
+            // btnRDP
+            // 
+            this.btnRDP.Location = new System.Drawing.Point(12, 50);
+            this.btnRDP.Name = "btnRDP";
+            this.btnRDP.Size = new System.Drawing.Size(237, 23);
+            this.btnRDP.TabIndex = 9;
+            this.btnRDP.Text = "RDP";
+            this.btnRDP.UseVisualStyleBackColor = true;
+            this.btnRDP.Click += new System.EventHandler(this.btnRDP_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 87);
+            this.Controls.Add(this.txtMachineName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnRDP);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMachineName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRDP;
+    }
+}
+
---
+++ LaunchWindowsRDPSample/LaunchWindowsRDPSample/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LaunchWindowsRDPSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Start_Windows_RDP()
+        {
+
+            String machine_name = txtMachineName.Text;
+
+            try
+            {
+
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                System.Security.SecureString ssPwd = new System.Security.SecureString();
+                proc.StartInfo.UseShellExecute = true;
+                proc.StartInfo.FileName = "mstsc.exe";
+                proc.StartInfo.Arguments = "/v:" + machine_name;
+                proc.Start();
+
+            }
+            catch
+            {
+
+            }
+
+
+
+        }
+
+        private void btnRDP_Click(object sender, EventArgs e)
+        {
+            Start_Windows_RDP();
+        }
+    }
+}
+
